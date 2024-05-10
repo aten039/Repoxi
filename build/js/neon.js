c0383b9ec4129fcaf17ebeb3a8e46da0b5b7cde6
aten039 <l.elvis039@gmail.com>
2024-05-09 23:22:46 -0400

    slider neon, banner neon y menu informacion neon

---
+++ build/js/neon.js
@@ -0,0 +1,2 @@
+function eventSlider(){let e=document.querySelectorAll(".slider-img");e.forEach((t=>{t.addEventListener("click",(function(){e.forEach((e=>e.classList.remove("active"))),t.classList.add("active")}))}))}window.addEventListener("DOMContentLoaded",(function(){eventSlider()}));
+//# sourceMappingURL=neon.js.map
